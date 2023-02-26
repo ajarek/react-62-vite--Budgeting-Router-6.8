55c16a4fbed3f7c4e0236a82a59dad256455d891
Ajarek <ajarek@poczta.onet.pl>
2023-02-26 23:51:15 +0100

    added react-toastify

--- src/actions/logout.jsx
+++ src/actions/logout.jsx
@@ -1,8 +1,10 @@
 import { redirect } from "react-router-dom"
 import { deleteItem } from "../helpers"
+import {toast} from "react-toastify"
 
 export  const logoutAction=async () =>{
   deleteItem('userName')
+  toast.success('You have deleted your account!')
   return redirect('/')
 
 }
