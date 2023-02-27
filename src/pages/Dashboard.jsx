56968395d449b41c0edb8c960f517a09f110f3dd
Ajarek <ajarek@poczta.onet.pl>
2023-02-28 00:44:46 +0100

    added AddBudgetForm

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,30 +1,43 @@
 import { useLoaderData } from 'react-router-dom'
 import { fetchData } from '../helpers'
 import { toast } from 'react-toastify'
+import AddBudgetForm from '../components/AddBudgetForm'
 import Intro from '../components/Intro'
 
 export const dashboardLoader = () => {
   const userName = fetchData('userName')
-  return { userName }
+  const budgets = fetchData('budgets')
+  return { userName, budgets }
 }
 
 export const dashboardAction = async({request}) => {
   const data = await request.formData()
   const formData = Object.fromEntries(data)
   try{
-//  localStorage.setItem('userName',JSON.stringify(formData.userName))
-//  return toast.success(`Welcome, ${formData.userName}`)
-throw new Error('There was a problem creating your')
+ localStorage.setItem('userName',JSON.stringify(formData.userName))
+ return toast.success(`Welcome, ${formData.userName}`)
+
 }catch(e){
- throw new Error('There was a problem creating your')
+ throw new Error('There was a problem creating your account.')
 }
 }
 
 const Dashboard = () => {
-  const { userName } = useLoaderData()
+  const { userName, budgets } = useLoaderData()
   return (
     <>
-      {userName?(<p>{userName}</p>):<Intro/>}     
+      {userName?(<div className='dashboard'>
+        <h1>Welcome back, <span>{userName}</span></h1>
+        <div className='grid-sm'>
+          {/* {budgets?('lol'):('lol1')} */}
+          <div className="grid-lg">
+            <div className="flex-lg">
+              <AddBudgetForm/>
+            </div>
+
+          </div>
+        </div>
+        </div>):<Intro/>}     
     </>
   )
 }
--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
-import { useRouteError, Link} from 'react-router-dom'
+import { useRouteError, Link, useNavigate} from 'react-router-dom'
 import {HomeIcon, ArrowUturnLeftIcon} from '@heroicons/react/24/solid'
 const Error = () => {
   const error= useRouteError()
+  const navigate =useNavigate()
   return (
     <div className='error'>
       <h1>Oh oh! We have a problem.</h1>
       <p>{error.message||error.statusText}</p>
       <div>
-        <button className='btn btn-dark'>
+        <button className='btn btn-dark' onClick={()=>navigate(-1)}>
           <ArrowUturnLeftIcon width={20}/>
           <span>Go Back</span>
         </button>
